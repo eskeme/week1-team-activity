31eccc06c5abb737092903ad0510e482ffcd5038
eskeme <hanibelle.justine@gmail.com>
2023-05-04 15:33:59 +0800

    week 3 assignment

--- src/js/product.js
+++ src/js/product.js
@@ -1,22 +1,5 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  let cartItems = getLocalStorage("so-cart") || [];
-  if (!Array.isArray(cartItems)) {
-    cartItems = [];
-  }
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+productDetails(productId);
